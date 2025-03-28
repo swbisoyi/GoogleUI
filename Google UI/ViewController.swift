798bc3ff31d4c9397952dbc86a25842c2fc11afe
Swagat <swagat.bisoyi@justdial.com>
2025-03-28 08:26:09 +0530

    Initial Commit

---
+++ Google UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google UI
+//
+//  Created by Swagat Kumar Bisoyi on 28/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Google UITests/Google_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Google_UITests.swift
+//  Google UITests
+//
+//  Created by Swagat Kumar Bisoyi on 28/03/25.
+//
+
+import Testing
+@testable import Google_UI
+
+struct Google_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
